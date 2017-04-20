d4a2e59b5a55f3921b76671f141f4f448e3f3e5f
Nick Piscopio <nick.piscopio@gmail.com>
2017-04-19 21:58:33 -0700

    Changed the services back to only sending status codes and data because we handle the response differently in Intencity now.

--- services/Response.php
+++ services/Response.php
@@ -12,18 +12,12 @@ class Response {
 	 */
 	function send($code, $data)
 	{
-		// [0] is the boolean for success.
-		// [1] is the status code.
-		$status = unserialize($code);
-		$response = array("STATUS" => array("SUCCESS" => $status[0], "CODE" => $status[1]), "DATA" => $data);
+		$response = array("STATUS_CODE" => $code, "DATA" => $data);
 
 		// Send the JSON response.
 		// The response should be as follows:
 		// {
-		//		"STATUS": {
-		//			"SUCCESS": true,
-		//			"CODE": 201	
-		//		},
+		//		"STATUS_CODE": 201,
 		//		"DATA": " [DATA TO SEND AS A RESPONSE] " 
 		// }
 		print json_encode($response);
--- services/status_codes.php
+++ services/status_codes.php
@@ -1,19 +1,17 @@
 <?php
 	/**
 	 * This file has all of the status code constants.
-	 *
-	 * We send an array for each for when we want a successful flag, but don't send data.
 	 */
 
 	// Success status codes.
-	define(RESPONSE_SUCCESS_ACCOUNT_CREATION, serialize(array(true, 201)));
-	define(RESPONSE_SUCCESS_ACCOUNT_UPDATED, serialize(array(true, 202)));
-	define(RESPONSE_SUCCESS_DATE_LOGIN_UPDATED, serialize(array(true, 203)));
-	define(RESPONSE_SUCCESS_PASSWORD_CHANGE, serialize(array(true, 204)));
+	define(RESPONSE_SUCCESS_ACCOUNT_CREATION, 201);
+	define(RESPONSE_SUCCESS_ACCOUNT_UPDATED, 202);
+	define(RESPONSE_SUCCESS_DATE_LOGIN_UPDATED, 203);
+	define(RESPONSE_SUCCESS_PASSWORD_CHANGE, 204);
 
 	// Failure status codes.
-	define(RESPONSE_FAILURE_EMAIL_ERROR, serialize(array(false, 501)));
-	define(RESPONSE_FAILURE_ACCOUNT_CREATION, serialize(array(false, 502)));
-	define(RESPONSE_FAILURE_PASSWORD_INVALID, serialize(array(false, 503)));
-	define(RESPONSE_FAILURE_PASSWORD_CHANGE, serialize(array(false, 504)));
+	define(RESPONSE_FAILURE_EMAIL_ERROR, 501);
+	define(RESPONSE_FAILURE_ACCOUNT_CREATION, 502);
+	define(RESPONSE_FAILURE_PASSWORD_INVALID, 503);
+	define(RESPONSE_FAILURE_PASSWORD_CHANGE, 504);
 ?>
