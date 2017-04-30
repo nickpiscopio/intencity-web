97d756100ccb188662781764892509f58bae8a5e
Nick Piscopio <nick.piscopio@gmail.com>
2017-04-30 10:35:06 -0700

    Updated the update priority service.

--- services/mobile/update_priority.php
+++ services/mobile/update_priority.php
@@ -4,24 +4,20 @@
 	 * 
 	 * Accepts:
 	 * 		inserts(0-n)	The equipment to insert to the database. Separate with a ",".
-	 *
- 	 * Returns:
- 	 *		SUCCESS
- 	 *		FAILURE
 	 * 
-	 * EXAMPLE URL	http://intencityapp.com/dev/services/mobile/update_exercise_priority.php?email=dev@gmail.com&exercises=Bicep Curl,Dead Lift,&priorities=50,-25
-	 * 
+	 * EXAMPLE URL	http://intencity.fit/dev/services/mobile/update_exercise_priority.php?user_id=100&exercises=1,2,&priorities=50,-25
 	 */
 
-	//Includes the database connection information.
+	// Includes the database connection information.
 	include_once '../db_connection.php';
 	include_once '../db_asset_names.php';
+	include_once '../status_codes.php';
+	include_once '../Response.php';
 
-	define("SUCCESS", "SUCCESS");
-	define("FAILURE", "FAILURE");
+	$response = new Response();
 	
-	//NEEDS TO BE CHANGED TO A POST.
-	$email = $_POST['email'];
+	// NEEDS TO BE CHANGED TO A POST.
+	$userId = $_POST['user_id'];
 	$exercises = $_POST['exercises'];
 	$priorities = $_POST['priorities'];
 
@@ -29,8 +25,8 @@
     $PRIORITY_LIMIT_LOWER = 0;
 	
 	$insertQuery = "";
-	$insertQuery .= "DELETE FROM  " . TABLE_EXCLUSION . " WHERE " . COLUMN_EMAIL . "=" . "'" . $email . "'; ";
-	$insertQuery .= "DELETE FROM  " . TABLE_EXERCISE_PRIORITY . " WHERE " . COLUMN_EMAIL . "=" . "'" . $email . "'; ";
+	$insertQuery .= "DELETE FROM " . TABLE_EXCLUSION . " WHERE " . COLUMN_USER_ID . "=" . $userId . "; ";
+	$insertQuery .= "DELETE FROM " . TABLE_EXERCISE_PRIORITY . " WHERE " . COLUMN_USER_ID . "=" . $userId . "; ";
 
 	if (isset($exercises))
 	{
@@ -45,26 +41,26 @@
 
 			if (($priority > $PRIORITY_LIMIT_LOWER && $priority < $PRIORITY_LIMIT_MID) || $priority > $PRIORITY_LIMIT_MID)
 			{
-				$insertQuery .= "INSERT INTO " . TABLE_EXERCISE_PRIORITY . " (" . COLUMN_EMAIL . "," . COLUMN_EXERCISE_NAME . ", " . COLUMN_PRIORITY . ") 
-								VALUES ('" . $email . "','" . $exercises[$i] . "'," . $priority . "); ";
+				$insertQuery .= "INSERT INTO " . TABLE_EXERCISE_PRIORITY . " (" . COLUMN_USER_ID . "," . COLUMN_EXERCISE_ID . ", " . COLUMN_PRIORITY . ") 
+								VALUES (" . $userId . ",'" . $exercises[$i] . "'," . $priority . "); ";
 			}
 			else if ($priority == $PRIORITY_LIMIT_LOWER)
 			{
-				$insertQuery .= "INSERT INTO " . TABLE_EXCLUSION . " (" . COLUMN_EMAIL . "," . COLUMN_EXCLUDE_FOREVER . ", " . COLUMN_EXCLUSION_NAME . ", " . COLUMN_EXCLUSION_TYPE . ") 
-								VALUES ('" . $email . "', 1, '" . $exercises[$i] . "', 'E'); ";
+				$insertQuery .= "INSERT INTO " . TABLE_EXCLUSION . " (" . COLUMN_USER_ID . "," . COLUMN_EXCLUDE_FOREVER . ", " . COLUMN_EXERCISE_ID . ", " . COLUMN_EXCLUSION_TYPE . ") 
+								VALUES (" . $userId . ", 1, '" . $exercises[$i] . "', 'E'); ";
 			}
 		}
 	}	
 
 	$query = mysqli_multi_query($conn, $insertQuery);
 
-	//Check if the query was successful.
+	// Check if the query was successful.
 	if($query)
 	{		
-		print json_encode(SUCCESS);
+		$response->send(STATUS_CODE_SUCCESS_EXERCISE_PRIORITY_UPDATED, NULL);
 	}
 	else
 	{
-		print json_encode(FAILURE);
+		$response->send(STATUS_CODE_FAILURE_EXERCISE_PRIORITY_UPDATE, NULL);
 	}
 ?>
